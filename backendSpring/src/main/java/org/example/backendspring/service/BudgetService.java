9cb8b7f269d4bfa47261d5a70127bae43e8aa636
Mohamed-Amiri <mohamedamiri281@gmail.com>
2025-05-14 10:47:41 +0100

    Your commit message

---
+++ backendSpring/src/main/java/org/example/backendspring/service/BudgetService.java
@@ -0,0 +1,49 @@
+package org.example.backendspring.service;
+
+import org.example.backendspring.entity.budget;
+import org.example.backendspring.repository.budgetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BudgetService {
+
+    private final budgetRepository repository;
+     @Autowired
+     public BudgetService(final budgetRepository repository){
+
+         this.repository = repository;
+     }
+
+    public  void saveBudget (budget budget){
+      repository.save(budget);
+
+    }
+
+     public budget getBudgetByID(long id){
+           return repository.findById(id).orElse(null);
+     }
+
+     public void  deletBudgetBYId(long id){
+        repository.deleteById(id);
+
+     }
+
+    public List<budget> getBudgets() {
+        return repository.findAll();
+    }
+    public Optional<budget> updatesBudget(long id,budget bg) {
+        return repository.findById(id)
+                .map(budget -> {
+                    budget.setMontant(bg.getMontant());
+                    budget.setEnum(bg.getEnum());
+                    budget.setDate(bg.getDate());
+                    return repository.save(budget);
+                });
+
+    }
+
+}
